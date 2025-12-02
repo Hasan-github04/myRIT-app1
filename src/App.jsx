588eb30596e28cd2314290131ab1e943e44b3e21
Hasan-github04 <ha4402@g.rit.edu>
2025-12-02 16:57:49 +0400

    completed all mycourses tabs

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import React from "react";
+import React, {useState} from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import { ThemeProvider } from "@/constants/ThemeContext";
 import TabsLayout from "@/layouts/TabsLayout";
-
+import Login from "@/app/auth/Login";
 // Screens
 import MyCoursesScreen from "@/app/tabs/layout";
 import NotificationsScreen from "@/app/tabs/notifications";
@@ -16,12 +16,31 @@ import CourseDetailScreen from "@/app/course/[id]";
 const queryClient = new QueryClient();
 
 export default function App() {
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+    const handleLogin = () => {
+      setIsAuthenticated(true);
+    };
+
+    const handleLogout = () => {
+      console.log("App: handleLogout called");
+      setIsAuthenticated(false);
+    };
+
+    if (!isAuthenticated) {
+    return <Login onLogin={handleLogin} />;
+  }
+
+  if (!isAuthenticated) {
+    return <Login onLogin={handleLogin} />;
+  }
+
   return (
     <QueryClientProvider client={queryClient}>
       <ThemeProvider>
         <BrowserRouter>
           <Routes>
-            <Route path="/" element={<TabsLayout />}>
+            <Route path="/" element={<TabsLayout onLogout={handleLogout}/>}>
               <Route index element={<MyCoursesScreen />} />
               <Route path="notifications" element={<NotificationsScreen />} />
               <Route path="sis" element={<SISScreen />} />
